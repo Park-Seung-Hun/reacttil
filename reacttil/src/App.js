b67ffddcd39b9b4118f6e47e33af94eb00c1a140
Seung Hun Park <psh19930326@gmail.com>
2021-09-07 16:13:35 +0900

    feat: add directory and path

--- reacttil/src/App.js
+++ reacttil/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import ClassComp from "./ClassComp";
-import Counter from "./Counter";
-import EventPractice from "./EventPractice";
-import EventPractice2 from "./EventPractice2";
-import FunctionComp from "./FunctionComp";
-import Say from "./Say";
+import ClassComp from "./ch2/ClassComp";
+import FunctionComp from "./ch2/FunctionComp";
+import Counter from "./ch3/Counter";
+import Say from "./ch3/Say";
+import EventPractice from "./ch4/EventPractice";
+import EventPractice2 from "./ch4/EventPractice2";
 
 function App() {
   return <EventPractice2 />;
