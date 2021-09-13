59bd36744c97cf3ef622cb29e9c335b277a97016
seunghun_park <seunghun_park2@tmax.co.kr>
2021-09-13 15:36:53 +0900

    TodoList Project Complete

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 import TodoTemplate from './component/TodoTemplate'
 import TodoInsert from './component/TodoInsert'
 import TodoList from './component/TodoList'
-import { useState } from 'react'
+import { useState, useRef, useCallback } from 'react'
 
 function App() {
   const [todos, setTodos] = useState([
@@ -12,10 +12,48 @@ function App() {
     { id: 3, text: '일정 3', checked: false },
   ])
 
+  // 고윳값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(4)
+
+  // 추가 기능
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      }
+      setTodos(todos.concat(todo)) // todos에 추가
+      nextId.current += 1
+    },
+    [todos]
+  )
+
+  // 삭제 기능
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id))
+    },
+    [todos]
+  )
+
+  // 수정 기능
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo
+        )
+      )
+    },
+    [todos]
+  )
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList todos={todos} />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   )
 }
--- todo-app/src/component/TodoListItem.jsx
+++ todo-app/src/component/TodoListItem.jsx
@@ -7,16 +7,17 @@ import {
 } from 'react-icons/md'
 import cn from 'classnames'
 
-const TodoListItem = ({ todo }) => {
-  const { text, checked } = todo
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+  const { id, text, checked } = todo
 
+  // classnames : cn은 1번쨰 파라미터는 그냥 추가, 2번쨰 파라미터의 값이 true일 경우에만 checked 클래스가 추가된다.
   return (
     <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })}>
+      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
       </div>
-      <div className="remove">
+      <div className="remove" onClick={() => onRemove(id)}>
         <MdRemoveCircleOutline />
       </div>
     </div>
